d6910f143881dd37e735f5ab139b084a9e25bef9
PauloFOJr <junior.pfo@gmail.com>
2026-02-23 10:01:56 -0300

    commit 0
    Exercícios de Stream: Binary Operator, Function, Predicate e Supplier.

---
+++ src/SupplierExample.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Representa uma operação que não aceita nenhuma argumento e retorna um resultado tipo T.
+ * É comumente usada para criar ou fornecer novos objetos de um determinado tipo.
+ */
+
+public class SupplierExample {
+    public static void main(String[] args) {
+        //Usar o supplier com expressão lambda para fornecer uma saudação personalizada
+        Supplier<String> saudacao = () -> "Olá, seja bem-vindo(a)!";
+
+        //Usar o supplier para obeter uma lista de 5 saudações
+        List<String> listaSaudacoes = Stream.generate(saudacao)
+                .limit(5)
+                .toList();
+        //imprimir as saudações geradas
+        //method reference
+        listaSaudacoes.forEach(System.out::println);
+    }
+}
